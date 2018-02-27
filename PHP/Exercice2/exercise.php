9ab1b2bd1908b0314946759939b87eb964b7b4cd
nathalie4499 <ivanmendoza@gmail.com>
2018-02-27 14:53:18 +0100

    addexo2php

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,19 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$longueur = strlen($password);
+var_dump($longueur);
+
+$password1length = floor(strlen($password)/2) + (strlen($password) %2);
+var_dump($password1length);
+$password2length = ceil(strlen($password)/2);
+var_dump($password2length);
+$passwordfirstpart = substr($password, 0, $password1length);
+var_dump($passwordfirstpart);
+
+$passwordsecondpart = substr($password, $password2length);
+var_dump($passwordsecondpart);
+
+$saltedpassword = $passwordfirstpart.$salt.$passwordsecondpart;
+var_dump($saltedpassword);
+
